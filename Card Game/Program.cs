770568adeacfad7451abcb9c28eed9c6d9340d17
Barnaby Kirkham <25087143@students.lincoln.ac.uk>
2022-05-18 02:40:00 +0100

    -

--- Card Game/Program.cs
+++ Card Game/Program.cs
@@ -14,7 +14,7 @@ namespace Dice_Game
             Player Player_1 = new Player(1,0);
             Player Player_2 = new Player(2,0);
             
-            Game game = new Game(0,0);
+            Game game = new Game(0);
 
             game.Play(Player_1);
             
